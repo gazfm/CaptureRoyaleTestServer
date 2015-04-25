935909a162b8e4623d7a4d1de1097e9045a5966b
Garry <gazfm@users.noreply.github.com>
2015-04-25 12:37:56 +0100

    Add simple castle model and migration

--- caproy/models.py
+++ caproy/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Castle(models.Model):
+    user = models.OneToOneField(User)
+    name = models.CharField(max_length=200)
+    definition = models.CharField(max_length=16384)
+
